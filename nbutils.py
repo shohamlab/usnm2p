9bb8f57ae9d2b5192ebf1abc1bf4bdbfef213ae8
Theo Lemaire <theo.lemaire1@gmail.com>
2022-01-06 11:49:21 -0500

    renamed analysis -> single_region_analysis and improved run_analysis script

---
+++ nbutils.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# @Author: Theo Lemaire
+# @Date:   2022-01-06 11:17:50
+# @Last Modified by:   Theo Lemaire
+# @Last Modified time: 2022-01-06 11:47:13
+
+import os
+import papermill as pm
+
+from logger import logger
+from batches import Batch
+
+''' Notebook running utilities '''
+
+def execute_notebook(pdict, input_nbpath, outdir):
+    '''
+    Wrapper around papermill's notebook execution function
+    
+    :param pdict: parameters dictionary (will be injected into the notebook)
+    :param input_nbpath: path to the input notebook
+    :param outdir: path to the output directory in which to save the output notebook
+    :return: path to the output notebook
+    '''
+    # Extract notebook name and extension
+    nbname, nbext = os.path.splitext(input_nbpath)
+    # Determine output notebook name from input and parameters
+    output_nbpath = os.path.join(outdir, f'{nbname}_{"_".join(pdict.values())}{nbext}')
+    # Execute notebook
+    logger.info(f'executing "{input_nbpath}" with parameters {pdict} as "{output_nbpath}"...')
+    pm.execute_notebook(input_nbpath, output_nbpath, parameters=pdict)
+    logger.info(f'{output_nbpath} notebook successfully executed')
+    return output_nbpath
+
+
+def execute_notebooks(pdicts, input_nbpath, outdir, **kwargs):
+    '''
+    Carry out multiple executions of a notebook with different input parameters combinations.
+
+    :param pdicts: list of parameters dictionary used for each notebook execution
+    :param input_nbpath: path to the input notebook
+    :param outdir: path to the output directory in which to save the output notebooks
+    :return: list of paths to the output notebooks
+    '''
+    # Establish batch queue with each parameter combination
+    queue = [list(x) for x in zip(*[pdicts, [input_nbpath] * len(pdicts), [outdir] * len(pdicts)])]
+    # Create and run job batch
+    batch = Batch(execute_notebook, queue)
+    nbpaths = batch.run(loglevel=logger.getEffectiveLevel(), **kwargs)
+    # Return output filepaths
+    return nbpaths
+
+
+class DirectorySwicther:
+    ''' Context manager ensuring temporary code inside a specified directory '''
+
+    def __init__(self, exec_dir):
+        self.exec_dir = exec_dir
+        self.call_dir = os.getcwd()
+        self.move = self.call_dir != self.exec_dir
+          
+    def __enter__(self):
+        if self.move:
+            logger.info(f'moving to "{self.exec_dir}"')
+            os.chdir(self.exec_dir)
+        return self
+      
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if self.move:
+            logger.info(f'moving back to "{self.call_dir}"')
+            os.chdir(self.call_dir)
+ 
--- run_analysis.py
+++ run_analysis.py
@@ -2,34 +2,22 @@
 # @Author: Theo Lemaire
 # @Date:   2021-12-29 12:43:46
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2022-01-04 18:12:37
+# @Last Modified time: 2022-01-06 11:48:01
 
 import os
 import logging
-import papermill as pm
 from argparse import ArgumentParser
+
 from fileops import get_data_root, get_date_mouse_region_combinations
-from batches import Batch
 from logger import logger
+from nbutils import DirectorySwicther, execute_notebooks
 
 logger.setLevel(logging.INFO)
 
-''' Utility script to run analysis notebook '''
-
-def execute_notebook(pdict, input_nbpath, outdir):
-    ''' Wrapper around papermill's notebook execution function '''
-    # Extract notebook name and extension
-    nbname, nbext = os.path.splitext(input_nbpath)
-    # Determine output notebook name from input and parameters
-    output_nbpath = os.path.join(outdir, f'{nbname}_{"_".join(pdict.values())}{nbext}')
-    # Execute notebook
-    logger.info(f'executing "{input_nbpath}" with parameters {pdict} as "{output_nbpath}"...')
-    pm.execute_notebook(input_nbpath, output_nbpath, parameters=pdict)
-    logger.info(f'{output_nbpath} notebook successfully executed')
-
+''' Utility script to run single region analysis notebook '''
 
 # Input & output
-input_nbpath = 'analysis.ipynb'  # path to input notebook 
+input_nbpath = 'single_region_analysis.ipynb'  # path to input notebook 
 outdir = 'outputs'  # relative path to output directory (w.r.t. this script)
 
 # Default parameters
@@ -40,27 +28,21 @@ defaults = {
     'date': '11122019'  # experiment date
 }
 
-
 if __name__ == '__main__':
-    # Get absolute path to script directory
-    script_fpath = os.path.realpath(__file__)
-    script_dir = os.path.split(script_fpath)[0]
 
-    # Moving to script directory if not there already
-    call_dir = os.getcwd()
-    if call_dir != script_dir:
-        logger.info(f'moving to "{script_dir}"')
-        os.chdir(script_dir)
-
-    # Extract input notebook and parameters from command line arguments
+    # Create command line parser
     parser = ArgumentParser()
     parser.add_argument('-i', '--input', default=input_nbpath, help='input notebook path')
+    parser.add_argument('-o', '--outdir', default=outdir, help='output directory path')
     for k, v in defaults.items():
         parser.add_argument(f'-{k[0]}', f'--{k}', default=v)
     parser.add_argument('--locate', default=False, action='store_true')
     parser.add_argument('--mpi', default=False, action='store_true')
+
+    # Extract input notebook, output diectory and execution parameters from command line arguments
     args = vars(parser.parse_args())
     input_nbpath = args.pop('input')
+    outdir = args.pop('outdir')
     locate = args.pop('locate')
     mpi = args.pop('mpi')
     if locate:
@@ -69,17 +51,16 @@ if __name__ == '__main__':
     else:
         # Otherwise, construct unique combination from inputs
         pdicts = [args]
+
+    # Set multiprocessing to False in case of single execution
     if len(pdicts) == 1:
         mpi = False
 
-    # Establish batch queue with each parameter combination
-    queue = [list(x) for x in zip(*[pdicts, [input_nbpath] * len(pdicts), [outdir] * len(pdicts)])]
+    # Get absolute path to directory of current file (where code must be executed)
+    script_fpath = os.path.realpath(__file__)
+    exec_dir = os.path.split(script_fpath)[0]
 
-    # Create and run job batch
-    batch = Batch(execute_notebook, queue)
-    batch.run(mpi=mpi, loglevel=logger.getEffectiveLevel(), ask_confirm=True)
-
-    # Moving back to calling directory if it was different
-    if call_dir != script_dir:
-        logger.info(f'moving back to "{call_dir}"')
-        os.chdir(call_dir)
+    # Execute notebooks within execution directory (to ensure correct function)
+    with DirectorySwicther(exec_dir) as ds:
+        # Execute notebooks as a batch with / without multiprocessing
+        output_nbpaths = execute_notebooks(pdicts, input_nbpath, outdir, mpi=mpi, ask_confirm=True)
