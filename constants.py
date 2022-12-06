fd528abd9d40ceb37f6b46114628dedcb56d2ca1
Theo Lemaire <theo.lemaire1@gmail.com>
2022-12-06 01:56:43 -0500

    offset analysis WIP

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@
 # @Author: Theo Lemaire
 # @Date:   2021-10-13 11:13:26
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2022-12-05 19:01:59
+# @Last Modified time: 2022-12-06 01:56:15
 
 ''' Collection of constants used throughout the code base. '''
 
@@ -100,7 +100,7 @@ EVENTS_BIN_INTERVAL = 1.4  # binning interval for events density quantification 
 
 # Artifacts
 VDISP_PEAK_THR = 2.  # threshold peak displacement velocity (um/s). Trials with peak velocities higher than this value get discarded 
-VDISP_AVG_THR = .7  # threshold average displacement velocity (um/s). Trials with average velocities higher than this value get discarded 
+VDISP_AVG_THR = 1.  # threshold average displacement velocity (um/s). Trials with average velocities higher than this value get discarded 
 PCT_PREACTIVE_THR = 50.  # threshold percentage of pre-active cells for each trial. Trials with higher percentages get discarded  
 NSTD_DEV_THR = 10  # number of standard deviations from timeseries distribution median outside which a trial is considered an outlier 
 MIN_VALID_TRIALS = 5  # minimum of avaliable valid trials to average from for the ROI-condition to be valid 
@@ -118,7 +118,7 @@ N_NEIGHBORS_PEAK = 1  # number of neighboring elements to consider to compute "a
 PTHR_DETECTION = 0.05  # significance threshold probability for activity detection in fluorescence signals
 PTHR_REJECTION = 0.01  # significance threshold probability for signal rejection
 PROP_CONDS_THR = 0.33  # minimum proportion of conditions with given response type for a cell to be classified as that same respone type
-OFFSET_MIN_PROP_POS = 0.5  # minimum proportion of positive responses in "best" condition to include datasets in offset analysis 
+OFFSET_MIN_PROP_POS = 0.33  # minimum proportion of positive responses in "best" condition to include datasets in offset analysis 
 
 ###################################### PARSING ######################################
 
