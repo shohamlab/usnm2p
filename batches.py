25e7716501fabfbb6cc47c7f8984b914c08666ce
Theo Lemaire <theo.lemaire1@gmail.com>
2023-10-11 19:29:12 -0400

    removed delay

--- batches.py
+++ batches.py
@@ -3,7 +3,7 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2017-08-22 14:33:04
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2023-10-11 19:18:19
+# @Last Modified time: 2023-10-11 19:29:08
 
 ''' Batch processing utilities '''
 
@@ -12,7 +12,6 @@ from datetime import datetime
 import multiprocessing as mp
 import numpy as np
 import pandas as pd
-import time
 
 from logger import logger
 
@@ -30,9 +29,6 @@ class Consumer(mp.Process):
 
     def run(self):
         while True:
-            # wait random delay between 0 and 1 s (to avoid ZMQ overload)
-            delay = np.random.uniform()
-            time.sleep(delay)
             nextTask = self.queue_in.get()
             if nextTask is None:
                 logger.debug(f'Exiting {self.name}')
